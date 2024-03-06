0dbeccf104462d09bbc1fdcd879c412d86097fb4
minataour <1njust1c104@gmail.com>
2024-03-06 20:31:57 +0530

    Setup Clerk API for auth and login. Add Clerk to app.

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div >
+      <p className="text-3xl font-bold">Hello Discord Clone</p>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+    </div>
+  );
+}
